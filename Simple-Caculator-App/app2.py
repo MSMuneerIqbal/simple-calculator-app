5201a4d73661afaf9c415f9cb00f565be14fe050
Muneer Iqbal <128808006+MSMuneerIqbal@users.noreply.github.com>
2024-09-19 02:06:25 +0500

    Update app2.py

--- Simple-Caculator-App/app2.py
+++ Simple-Caculator-App/app2.py
@@ -1,42 +1,53 @@
+# check demo on this link "https://simple-cal.streamlit.app/"
 import streamlit as st
 
-# Title of the app
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return None
+
 st.title("Simple Calculator")
 
-# Input field for user's name
 user_name = st.text_input("Enter your name")
 
-# Initialize session state to store result
 if 'result' not in st.session_state:
     st.session_state.result = None
 
-# Input fields for two numbers
 num1 = st.number_input("Enter first number", value=0.0)
 num2 = st.number_input("Enter second number", value=0.0)
 
-# Create a row of buttons for each operation
 col1, col2, col3, col4, col5 = st.columns(5)
 
 with col1:
     if st.button("Add"):
-        st.session_state.result = f"Dear {user_name} your result is = {num1 + num2}"
+        st.session_state.result = f"Dear {user_name} your result is = {add(num1, num2)}"
 with col2:
     if st.button("Subtract"):
-        st.session_state.result = f"Dear {user_name} your result is = {num1 - num2}"
+        st.session_state.result = f"Dear {user_name} your result is = {subtract(num1, num2)}"
 with col3:
     if st.button("Multiply"):
-        st.session_state.result = f"Dear {user_name} your result is = {num1 * num2}"
+        st.session_state.result = f"Dear {user_name} your result is = {multiply(num1, num2)}"
 with col4:
     if st.button("Divide"):
-        if num2 != 0:
-            st.session_state.result = f"Dear {user_name} your result is = {num1 / num2}"
+        result = divide(num1, num2)
+        if result is not None:
+            st.session_state.result = f"Dear {user_name} your result is = {result}"
         else:
             st.session_state.result = f"Dear {user_name} We Cannot divide by zero"
 with col5:
     if st.button("Clear"):
         st.session_state.result = "Click Any Button get Result"
 
-# Display the result in the center with font size 20 and red color
 if st.session_state.result is not None:
     st.markdown(
         f'<p style="font-size: 50px; color: red; text-align: center;">{st.session_state.result}</p>',
